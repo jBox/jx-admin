e40898307076f6837d052e554c243075d756d466
Junhua Wei <junhua@outlook.com>
2018-04-19 19:43:58 +0800

    sch

---
+++ src/components/Widgets/OrderOperation.js
@@ -0,0 +1,250 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames";
+import styles from "./OrderPreview.css";
+import Form from "../Form";
+import Autocomplete from "../Form/Autocomplete";
+import { LPN_CONTENT_PATTERN, SERVICE_STATUS } from "../utils";
+
+import Button from "../Form/Button";
+
+class Scheduler extends Component {
+    static propTypes = {
+        vehicles: PropTypes.array,
+        drivers: PropTypes.array,
+        order: PropTypes.object,
+        onSchedule: PropTypes.func
+    }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            reschedule: false
+        }
+
+        this.schedule = props.order.schedule ?
+            {
+                vehicle: { number: props.order.schedule.license },
+                driver: { ...props.order.schedule.driver }
+            } : {
+                driver: null,
+                vehicle: null
+            };
+    }
+
+    handleReSchedule = (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+        this.setState({ reschedule: true });
+    }
+
+    submitButton = () => {
+        const { order } = this.props;
+        if (order.status.id === "confirmed" || this.state.reschedule) {
+            return (<Button type="submit" block success>执行调度</Button>);
+        }
+
+        if (!this.state.reschedule && order.status.id === "scheduled") {
+            return (<Button onClick={this.handleReSchedule} block warning>重新调度</Button>);
+        }
+
+        return null;
+    }
+
+    handleSchedule = () => {
+        const { order, onSchedule } = this.props;
+        if (onSchedule) {
+            onSchedule(order, this.schedule);
+        }
+        this.setState({ reschedule: false });
+    }
+
+    handleVehicleChange = (item) => {
+        const [number, model] = item.split(/[,|，]/);
+        this.schedule.vehicle = {
+            number: number ? number.trim() : "",
+            model: model ? model.trim() : ""
+        };
+    }
+
+    handleDriverChange = (item) => {
+        const [title, mobile] = item.split(/[,|，]/);
+        this.schedule.driver = {
+            title: title ? title.trim() : "",
+            mobile: mobile ? mobile.trim() : ""
+        };
+    }
+
+    render() {
+        const { order, vehicles, drivers, children } = this.props;
+        const vehicleItems = vehicles.map((item) => ({
+            id: item.number,
+            label: item.number,
+            description: item.model
+        }));
+        const driverItems = drivers.map((item) => {
+            const id = item.mobile;
+            return { id, label: item.title || item.nickname, description: id };
+        });
+
+        const showForm = order.status.id === "confirmed" || this.state.reschedule;
+
+        const defaultSchedule = { vehicle: "", driver: "" };
+        if (order.schedule /*license, driver:{mobile,title}*/) {
+            const v = vehicles.find(x => x.number === order.schedule.license);
+            if (v) {
+                defaultSchedule.vehicle = `${v.number}, ${v.model}`;
+            }
+            defaultSchedule.driver = `${order.schedule.driver.title}, ${order.schedule.driver.mobile}`;
+        }
+
+        return (
+            <Form className="row" onSubmit={this.handleSchedule}>
+                {showForm && (<div className="col-md-4 col-sm-12">
+                    <Autocomplete items={vehicleItems}
+                        value={defaultSchedule.vehicle}
+                        name="vehicle"
+                        placeholder="车牌号, 车型"
+                        pattern={`^${LPN_CONTENT_PATTERN}\\s*[,|，]\\s*(商务车|轿车)$`}
+                        message="请输入车牌号与车型"
+                        required
+                        onChange={this.handleVehicleChange}
+                    />
+                </div>)}
+                {showForm && (<div className="col-md-4 col-sm-12">
+                    <Autocomplete items={driverItems}
+                        value={defaultSchedule.driver}
+                        name="driver"
+                        pattern={`^[^\\d]+\\s*[,|，]\\s*1\\d{10}$`}
+                        placeholder="司机称呼，手机号码"
+                        message="请输入司机称呼与手机号码"
+                        required
+                        onChange={this.handleDriverChange}
+                    />
+                </div>)}
+                <div className="col-md-4 col-sm-12">
+                    {this.submitButton()}
+                </div>
+                {children}
+            </Form>
+        );
+    }
+}
+
+export default class OrderOperation extends Component {
+    static defaultProps = {
+        vehicles: [],
+        drivers: []
+    }
+
+    static propTypes = {
+        vehicles: PropTypes.array,
+        drivers: PropTypes.array,
+        order: PropTypes.object,
+        onConfirm: PropTypes.func,
+        onConfirmCancel: PropTypes.func,
+        onSchedule: PropTypes.func,
+        onDepart: PropTypes.func,
+        onRevert: PropTypes.func,
+        onComplete: PropTypes.func
+    }
+
+    confirmCancel = () => {
+        const { onConfirmCancel } = this.props;
+        return (
+            <div className="box-footer">
+                <div className="row">
+                    <div className="col-md-offset-8 col-md-4 col-sm-12">
+                        <Button onClick={onConfirmCancel} block danger>确认取消订单</Button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+    confirm = () => {
+        const { onConfirm } = this.props;
+        return (
+            <div className="box-footer">
+                <div className="row">
+                    <div className="col-md-offset-8 col-md-4 col-sm-12">
+                        <Button onClick={this.onConfirm} block primary>确认订单</Button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+    scheduleAndDepart = () => {
+        const { order, vehicles, drivers, onSchedule, onDepart } = this.props;
+        return (
+            <div className="box-footer">
+                <Scheduler
+                    order={order}
+                    vehicles={vehicles}
+                    drivers={drivers}
+                    onSchedule={onSchedule}
+                >
+                    {order.status.id === "scheduled" && (
+                        <div className="col-md-4 col-sm-12">
+                            <Button onClick={onDepart} block danger>确认发车</Button>
+                        </div>
+                    )}
+                </Scheduler>
+            </div>
+        );
+    }
+
+    revert = () => {
+        const { onRevert } = this.props;
+        return (
+            <div className="box-footer">
+                <div className="row">
+                    <div className="col-md-offset-8 col-md-4 col-sm-12">
+                        <Button onClick={onRevert} block danger>确认收车</Button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+    complete = () => {
+        const { onComplete } = this.props;
+        return (
+            <div className="box-footer">
+                <div className="row">
+                    <div className="col-md-offset-8 col-md-4 col-sm-12">
+                        <Button onClick={onComplete} block success>完成</Button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+    render() {
+
+        const { order } = this.props;
+
+        if (order.service.status === "cancelling") {
+            return this.confirmCancel();
+        }
+
+        if (order.status.id === "submitted") {
+            return this.confirm();
+        }
+
+        if (order.status.id === "confirmed" || order.status.id === "scheduled") {
+            return this.scheduleAndDepart();
+        }
+
+        if (order.status.id === "departure") {
+            return this.revert();
+        }
+
+        if (order.status.id === "reverted") {
+            return this.complete();
+        }
+
+        return null;
+    }
+}
--- src/components/Widgets/OrderPreview.js
+++ src/components/Widgets/OrderPreview.js
@@ -4,9 +4,10 @@ import classNames from "classnames";
 import styles from "./OrderPreview.css";
 import Form from "../Form";
 import Autocomplete from "../Form/Autocomplete";
-import { LPN_CONTENT_PATTERN } from "../utils";
+import { LPN_CONTENT_PATTERN, SERVICE_STATUS } from "../utils";
 
 import Button from "../Form/Button";
+import OrderOperation from "./OrderOperation";
 
 const VehicleItem = ({ model, count, withDriver, notes }) => {
     const additional = [`${count} 辆`];
@@ -33,10 +34,13 @@ const OrderStatus = ({ order }) => {
     const traces = order.traces.reduceRight((items, item) => {
         return items.concat({ state: item.state, time: item.time.toDateTime() });
     }, []);
+    const status = order.service.status ?
+        SERVICE_STATUS[order.service.status] :
+        order.status.label;
     return (
         <div className="box box-widget box-solid collapsed-box" style={{ boxShadow: "none" }}>
             <div className="box-header">
-                <span>订单状态：<label>{order.status.label}</label></span>
+                <span>订单状态：<label>{status}</label></span>
 
                 <div className="box-tools pull-right">
                     <button type="button" className="btn btn-box-tool" data-widget="collapse">查看详情 &gt;</button>
@@ -59,27 +63,17 @@ export default class OrderPreview extends Component {
         vehicles: PropTypes.array,
         drivers: PropTypes.array,
         order: PropTypes.object,
+        onModify: PropTypes.func,
         onConfirm: PropTypes.func,
-        onSchedule: PropTypes.func,
         onConfirmCancel: PropTypes.func,
-        onComplete: PropTypes.func,
-        onModify: PropTypes.func
-    }
-
-    schedule = {
-        driver: null,
-        vehicle: null
+        onSchedule: PropTypes.func,
+        onDepart: PropTypes.func,
+        onRevert: PropTypes.func,
+        onComplete: PropTypes.func
     }
 
     componentDidMount() {
         jQuery(".box").boxWidget();
-    }
-
-    handleConfirm = () => {
-        const { order, onConfirm } = this.props;
-        if (onConfirm) {
-            onConfirm(order);
-        }
     }
 
     handleModify = () => {
@@ -87,129 +81,6 @@ export default class OrderPreview extends Component {
         if (onModify) {
             onModify(order);
         }
-    }
-
-    handleSchedule = () => {
-        console.log(this.schedule);
-        const { order, onSchedule } = this.props;
-        if (onSchedule) {
-            onSchedule(order, this.schedule);
-        }
-    }
-
-    handleConfirmCancel = () => {
-        const { order, onConfirmCancel } = this.props;
-        if (onConfirmCancel) {
-            onConfirmCancel(order);
-        }
-    }
-
-    handleComplete = () => {
-        const { order, onComplete } = this.props;
-        if (onComplete) {
-            onComplete(order);
-        }
-    }
-
-    handleVehicleChange = (item) => {
-        const [number, model] = item.split(/[,|，]/);
-        this.schedule.vehicle = {
-            number: number ? number.trim() : "",
-            model: model ? model.trim() : ""
-        };
-    }
-
-    handleDriverChange = (item) => {
-        const [title, mobile] = item.split(/[,|，]/);
-        this.schedule.driver = {
-            title: title ? title.trim() : "",
-            mobile: mobile ? mobile.trim() : ""
-        };
-    }
-
-    renderOperation = () => {
-
-        const { order, vehicles, drivers } = this.props;
-
-        if (order.status.id === "submitted") {
-            return (
-                <div className="box-footer">
-                    <div className="row">
-                        <div className="col-md-offset-8 col-md-4 col-sm-12">
-                            <Button onClick={this.handleConfirm} block primary>确认订单</Button>
-                        </div>
-                    </div>
-                </div>
-            );
-        }
-
-        if (order.status.id === "confirmed") {
-            const vehicleItems = vehicles.map((item) => ({ id: item.number, label: item.number, description: item.model }));
-            const driverItems = drivers.map((item) => {
-                const id = item.mobile;
-                return { id, label: item.title || item.nickname, description: id };
-            });
-            return (
-                <div className="box-footer">
-                    <Form className="row" onSubmit={this.handleSchedule}>
-                        <div className="col-md-4 col-sm-12">
-                            <Autocomplete items={vehicleItems}
-                                name="vehicle"
-                                placeholder="车牌号, 车型"
-                                pattern={`^${LPN_CONTENT_PATTERN}\\s*[,|，]\\s*(商务车|轿车)$`}
-                                message="请输入车牌号与车型"
-                                required
-                                onChange={this.handleVehicleChange}
-                            />
-                        </div>
-                        <div className="col-md-4 col-sm-12">
-                            <Autocomplete items={driverItems}
-                                name="driver"
-                                pattern={`^[^\\d]+\\s*[,|，]\\s*1\\d{10}$`}
-                                placeholder="司机称呼，手机号码"
-                                message="请输入司机称呼与手机号码"
-                                required
-                                onChange={this.handleDriverChange}
-                            />
-                        </div>
-                        <div className="col-md-4 col-sm-12">
-                            <Button type="submit" block success>执行调度</Button>
-                        </div>
-                    </Form>
-                </div>
-            );
-        }
-
-        if (order.status.id === "cancelling") {
-            return (
-                <div className="box-footer">
-                    <div className="row">
-                        <div className="col-md-offset-8 col-md-4 col-sm-12">
-                            <Button onClick={this.handleConfirmCancel} block danger>确认取消订单</Button>
-                        </div>
-                    </div>
-                </div>
-            );
-        }
-
-        if (order.status.id === "scheduled") {
-
-            const duration = Number(order.duration);
-            const departureTime = new Date(order.departureTime);
-            const endTime = departureTime.setDate(departureTime.getDate() + duration);
-            const disabled = endTime > Date.now();
-            return (
-                <div className="box-footer">
-                    <div className="row">
-                        <div className="col-md-offset-8 col-md-4 col-sm-12">
-                            <Button onClick={this.handleComplete} disabled={disabled} block success>完成</Button>
-                        </div>
-                    </div>
-                </div>
-            );
-        }
-
-        return null;
     }
 
     getBoxStyle = () => {
@@ -235,7 +106,6 @@ export default class OrderPreview extends Component {
         const { order } = this.props;
 
         return (
-
             <div className={classNames("box", this.getBoxStyle())}>
                 <div className="box-header with-border">
                     <div className="user-block">
@@ -268,7 +138,7 @@ export default class OrderPreview extends Component {
 
                 <OrderStatus order={order} />
 
-                {this.renderOperation()}
+                <OrderOperation {...this.props} />
             </div>
         );
     }
